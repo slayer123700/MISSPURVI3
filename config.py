5e0b6f17aa1c9c399f7b02d1cc60495660880a26
ramrk399 <ramavtarchauhan2000@gmail.com>
2024-08-14 17:30:14 +0530

    Update config.py
    Signed-off-by: ramrk399 <ramavtarchauhan2000@gmail.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/istkharalam62/MISSPURVIV2",
+    "https://github.com/ramrk399/MISSPURVI3",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(

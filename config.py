bcc531e865982d2d02d79ee6809c4795503f1e8b
nonsecularman <krishbharti3404@gmail.com>
2026-05-05 23:12:23 +0530

    Update config.py
    Signed-off-by: nonsecularman <krishbharti3404@gmail.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ramrk399/MISSPURVI3",
+    "https://github.com/slayer123700/MISSPURVI3",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
